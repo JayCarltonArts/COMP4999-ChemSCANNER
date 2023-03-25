31d7caffa49616eac22d616f49adbcad690ef91e
eloi-torres <70127014+eloi-torres@users.noreply.github.com>
2023-03-24 23:45:05 -0400

    Update SCANNER TEST.py

--- SCANNER TEST.py
+++ SCANNER TEST.py
@@ -32,6 +32,11 @@ def t_IDCONT(t):
 def t_TIPO(t):
     r'modelo'
     return t
+def t_OPERACION (t):
+    r'graficar2d|graficar3d|pesomolecular'
+    t.value=str(t.value)
+    return t
+
 def t_ELEMENTO_QUIMICO(t):
     r'H|Li|Na|K|Rb|Cs|Fr|Be|Mg|Ca|Sr|Ba|Ra|Sc|Y|Ti|Zr|Hf|Db|V|Nb|Ta|Jl|Cr|Mo|W|Rf|Mn|Tc|Re|Bh|Fe|Ru|Os|Hn|Co|Rh|Ir|Mt|Ni|Pd|Pt|Cu|Ag|Au|Zn|Cd|Hg|B|Al|Ga|ln|Tl|C|Si|Ge|Sn|Pb|N|P|As|Sb|Bi|O|S|Se|Te|Po|F|Cl|Br|I|At|He|Ne|Ar|Kr|Xe|Rn'
     t.value=str(t.value)
@@ -49,6 +54,45 @@ def t_VALENCIA(t):
     r'[1-9]'#Any digit from 0-9
     t.value = int(t.value)
     return t
+
+def t_ELEMENTO(t):
+     t_ELEMENTO_QUIMICO|t_ELEMENTO_QUIMICO (t_VALENCIA)
+     return t
+
+def t_SENTENCIA (t):
+    r'defina ({t_ID}) como ({t_TIPO}|{t_ID}) = ({t_MODELO_MOLECULAR} | {t_OPERACION}) ({ID})'
+    return t
+
+def t_SENTENCIAS (t):
+    t_SENTENCIA (t_FIN_DE_LINEA)(t_SENTENCIAS) | t_SENTENCIA(t_FIN_DE_LINEA)
+
+def t_GRUPO_FUNCIAL_INFERIOR (t):
+    r'[{t_MODELO_GRUPO_FUNCIONAL}]'
+    return t
+
+def t_GRUPO_FUNCIONAL_SUPERIOR (t):
+    r'({t_MODELO_GRUPO_FUNCIONAL})'
+    return t
+
+def t_GRUPO_FUNCIONAL(t):
+    r'{t_GRUPO_FUNCIONAL_INFERIOR(t_GRUPO_FUNCIONAL_SUPERIOR)} | {t_GRUPO_FUNCIONAL_SUPERIOR(t_GRUPO_FUNCIONAL_INFERIOR)} | ({t_MODELO_GRUPO_FUNCIONAL}) | [{t_MODELO_GRUPO_FUNCIONAL}]'
+    return t
+
+def t_MODELO_GRUPO_FUNCIONAL (t):
+    r'{t_ENLACE (t_MODELO_MOLECULAR)} | {t_ELEMENTO_QUIMICO} | {t_ELEMENTO_QUIMICO (t_VALENCIA)} | {t_ELEMENTO(t_GRUPO_FUNCIONAL)} | {t_COMPUESTO(t_ELEMENTO)} | {t_COMPUESTO(t_ELEMENTO)(t_GRUPO_FUNCIONAL)} | {t_COMPUESTO(t_COMPUESTO)(t_COMPUESTOS)}'
+    return t
+
+def t_MODELO_MOLECULAR (t):
+    r'{t_ELEMENTO_QUIMICO} | {t_ELEMENTO_QUIMICO (t_VALENCIA)} | {t_ELEMENTO(t_GRUPO_FUNCIONAL)} | {t_COMPUESTO(t_GRUPO_FUNCIONAL)} | {t_COMPUESTO(t_ELEMENTO)(t_GRUPO_FUNCIONAL)} | {t_COMPUESTO(t_COMPUESTO)(t_COMPUESTOS)}'
+    return t
+
+def t_COMPUESTOS (t):
+    r'{COMPUESTO (COMPUESTOS)} | {COMPUESTO}'
+    return t
+def t_COMPUESTO(t):
+    r'{t_ELEMENTO_QUIMICO} | {t_ELEMENTO_QUIMICO (t_VALENCIA)} | {t_ELEMENTO (t_GRUPO_FUNCIONAL)} | {t_ELEMENTO (t_GRUPO_FUNCIONAL)(t_ENLACE)} |{t_ELEMENTO (t_ENLACE)}'
+    return t
+
 # Define a rule to handle whitespace
 t_ignore = ' \t\n'
 
