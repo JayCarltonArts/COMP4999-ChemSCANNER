c47e7e7b9626bb884773d4a754f9eff775fbc79b
JayCarltonArts <99850395+JayCarltonArts@users.noreply.github.com>
2023-03-25 19:48:28 -0400

    part 1 of trying to make it work

--- SCANNER TEST.py
+++ SCANNER TEST.py
@@ -12,6 +12,24 @@ tokens = (
     'ENLACE',
     'FIN_DE_LINEA',
     'ELEMENTO_QUIMICO'
+    'DECLARACION_DE_VARIABLE',
+    'OPERACION_CON_MODELO',
+    'OPERACION',
+    'GRAFICAR',
+    'PROPIEDADES_FISICAS',
+    'SENTENCIA',
+    'SENTENCIAS',
+    'DEFINICION_DEL_MODELO',
+    'MODELO_MOLECULAR',
+    'ELEMENTO',
+    'CADENA_MOLECULAR',
+    'CADENA_ATOMICA',
+    'GRUPO_FUNCIONAL',
+    'GRUPO_FUNCIONAL_SUPERIOR',
+    'GRUPO_FUNCIONAL_INFERIOR',
+    'COMPUESTO',
+    'COMPUESTOS',
+    'MODELO_GRUPO_FUNCIONAL'
 )
 
 # Define the regular expression for each token
@@ -19,84 +37,61 @@ t_FIN_DE_LINEA=r'\;|\:'
 t_ENLACE=r'\;|:=|\='
 
 # Define a regular expression 
-def t_ID(t):
-    r'{t_LETRA}|{t_LETRA}({t_IDCONT})*'
-    t.value = str(t.value)
-    return t
-def t_IDCONT(t):
-    r'{t_LETRA}({LETRA}(?P<idcont>{t_IDCONT})|{DIGITO}(?P<idcont>{t_IDCONT}))*'
-    t.value = str(t.value)
-    return t
-def t_TIPO(t):
-    r'modelo'
-    return t
-def t_OPERACION (t):
-    r'graficar2d|graficar3d|pesomolecular'
-    t.value=str(t.value)
-    return t
+t_LETRA= r'\w+'
+t_DIGITO=r'\d+'#Any digit from 0-9
+t_ELEMENTO_QUIMICO= r'H|Li|Na|K|Rb|Cs|Fr|Be|Mg|Ca|Sr|Ba|Ra|Sc|Y|Ti|Zr|Hf|Db|V|Nb|Ta|Jl|Cr|Mo|W|Rf|Mn|Tc|Re|Bh|Fe|Ru|Os|Hn|Co|Rh|Ir|Mt|Ni|Pd|Pt|Cu|Ag|Au|Zn|Cd|Hg|B|Al|Ga|ln|Tl|C|Si|Ge|Sn|Pb|N|P|As|Sb|Bi|O|S|Se|Te|Po|F|Cl|Br|I|At|He|Ne|Ar|Kr|Xe|Rn'
+t_TIPO= r'modelo'
+t_OPERACION=r'graficar2d|graficar3d|pesomolecular'
+t_VALENCIA= r'[1-9]'#Any digit from 0-9
+t_IDCONT = t_LETRA + r'|' + r'(' + t_LETRA + r')' + t_LETRA + r'|' + t_DIGITO + r'|' + r'(' + t_DIGITO + r')' + t_LETRA 
+t_ID = t_LETRA + r'|' + r'(' + t_LETRA + r')' + t_IDCONT
 
-def t_LETRA(t):
-    r'[a-zA-Z]'
-    t.value=str(t.value)
-    return t
-
-def t_ELEMENTO_QUIMICO(t):
-    r'H|Li|Na|K|Rb|Cs|Fr|Be|Mg|Ca|Sr|Ba|Ra|Sc|Y|Ti|Zr|Hf|Db|V|Nb|Ta|Jl|Cr|Mo|W|Rf|Mn|Tc|Re|Bh|Fe|Ru|Os|Hn|Co|Rh|Ir|Mt|Ni|Pd|Pt|Cu|Ag|Au|Zn|Cd|Hg|B|Al|Ga|ln|Tl|C|Si|Ge|Sn|Pb|N|P|As|Sb|Bi|O|S|Se|Te|Po|F|Cl|Br|I|At|He|Ne|Ar|Kr|Xe|Rn'
-    t.value=str(t.value)
-    return t
-
-def t_DIGITO(t):
-    r'\d'#Any digit from 0-9
-    t.value =int(t.value)
-    return t
-
-def t_VALENCIA(t):
-    r'[1-9]'#Any digit from 0-9
-    t.value = int(t.value)
-    return t
-
-def t_ELEMENTO(t):
-     t_ELEMENTO_QUIMICO|t_ELEMENTO_QUIMICO (t_VALENCIA)
-     return t
-
-def t_SENTENCIA (t):
-    r'defina ({t_ID}) como ({t_TIPO}|{t_ID}) = ({t_MODELO_MOLECULAR} | {t_OPERACION}) ({ID})'
-    return t
-
-def t_SENTENCIAS (t):
-    t_SENTENCIA (t_FIN_DE_LINEA)(t_SENTENCIAS) | t_SENTENCIA(t_FIN_DE_LINEA)
-
-def t_GRUPO_FUNCIAL_INFERIOR (t):
-    r'[{t_MODELO_GRUPO_FUNCIONAL}]'
-    return t
-
-def t_GRUPO_FUNCIONAL_SUPERIOR (t):
-    r'({t_MODELO_GRUPO_FUNCIONAL})'
-    return t
-
-def t_GRUPO_FUNCIONAL(t):
-    r'{t_GRUPO_FUNCIONAL_INFERIOR(t_GRUPO_FUNCIONAL_SUPERIOR)} | {t_GRUPO_FUNCIONAL_SUPERIOR(t_GRUPO_FUNCIONAL_INFERIOR)} | ({t_MODELO_GRUPO_FUNCIONAL}) | [{t_MODELO_GRUPO_FUNCIONAL}]'
-    return t
-
-def t_MODELO_GRUPO_FUNCIONAL (t):
-    r'{t_ENLACE (t_MODELO_MOLECULAR)} | {t_ELEMENTO_QUIMICO} | {t_ELEMENTO_QUIMICO (t_VALENCIA)} | {t_ELEMENTO(t_GRUPO_FUNCIONAL)} | {t_COMPUESTO(t_ELEMENTO)} | {t_COMPUESTO(t_ELEMENTO)(t_GRUPO_FUNCIONAL)} | {t_COMPUESTO(t_COMPUESTO)(t_COMPUESTOS)}'
-    return t
-
-def t_MODELO_MOLECULAR (t):
-    r'{t_ELEMENTO_QUIMICO} | {t_ELEMENTO_QUIMICO (t_VALENCIA)} | {t_ELEMENTO(t_GRUPO_FUNCIONAL)} | {t_COMPUESTO(t_GRUPO_FUNCIONAL)} | {t_COMPUESTO(t_ELEMENTO)(t_GRUPO_FUNCIONAL)} | {t_COMPUESTO(t_COMPUESTO)(t_COMPUESTOS)}'
-    return t
-
-def t_COMPUESTOS (t):
-    r'{COMPUESTO (COMPUESTOS)} | {COMPUESTO}'
-    return t
-def t_COMPUESTO(t):
-    r'{t_ELEMENTO_QUIMICO} | {t_ELEMENTO_QUIMICO (t_VALENCIA)} | {t_ELEMENTO (t_GRUPO_FUNCIONAL)} | {t_ELEMENTO (t_GRUPO_FUNCIONAL)(t_ENLACE)} |{t_ELEMENTO (t_ENLACE)}'
-    return t
+    
 
 # Define a rule to handle whitespace
 t_ignore = ' \t\n'
 
 # Definir las reglas de produccion
+t_ELEMENTO= t_ELEMENTO_QUIMICO + r'|' + t_ELEMENTO_QUIMICO + r'(' + t_VALENCIA + r')'
+t_COMPUESTO=r'{t_ELEMENTO_QUIMICO} | {t_ELEMENTO_QUIMICO (t_VALENCIA)} | {t_ELEMENTO (t_GRUPO_FUNCIONAL)} | {t_ELEMENTO (t_GRUPO_FUNCIONAL)(t_ENLACE)} |{t_ELEMENTO (t_ENLACE)}'
+
+t_COMPUESTOS= t_COMPUESTO + r'('+ t_COMPUESTOS +r')'+ r'|'+ t_COMPUESTO
+
+
+t_GRUPO_FUNCIONAL=r'{t_GRUPO_FUNCIONAL_INFERIOR(t_GRUPO_FUNCIONAL_SUPERIOR)} | {t_GRUPO_FUNCIONAL_SUPERIOR(t_GRUPO_FUNCIONAL_INFERIOR)} | ({t_MODELO_GRUPO_FUNCIONAL}) | [{t_MODELO_GRUPO_FUNCIONAL}]'
+
+t_MODELO_MOLECULAR= t_ELEMENTO_QUIMICO + r'|' + t_ELEMENTO_QUIMICO + r'(' + t_VALENCIA + r')' + r'|' + t_ELEMENTO + r'(' + t_GRUPO_FUNCIONAL + r')' + r'|' + t_COMPUESTO + r'(' + t_GRUPO_FUNCIONAL+r')'+ r'|'+ t_COMPUESTO+ r'(' + t_ELEMENTO + r')' +r'('+ t_GRUPO_FUNCIONAL + r')'+ r'|'+ t_COMPUESTO + r'(' + t_COMPUESTO + r')' + r'(' + t_COMPUESTOS + r')'
+ 
+t_MODELO_GRUPO_FUNCIONAL=t_ENLACE + (t_MODELO_MOLECULAR) | {t_ELEMENTO_QUIMICO} | {t_ELEMENTO_QUIMICO (t_VALENCIA)} | {t_ELEMENTO(t_GRUPO_FUNCIONAL)} | {t_COMPUESTO(t_ELEMENTO)} | {t_COMPUESTO(t_ELEMENTO)(t_GRUPO_FUNCIONAL)} | {t_COMPUESTO(t_COMPUESTO)(t_COMPUESTOS)}'
+
+t_GRUPO_FUNCIAL_INFERIOR= r'[' + t_MODELO_GRUPO_FUNCIONAL + r']'
+
+t_GRUPO_FUNCIONAL_SUPERIOR= r'(' + t_MODELO_GRUPO_FUNCIONAL + r')'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+t_SENTENCIA= r'defina' + r'(' + t_ID + r')' + r'como' + r'(' + t_TIPO + r'|' + t_ID + r')' + r'=' + r'('+ t_MODELO_MOLECULAR + r'|' {t_OPERACION}) ({ID})'
+
+t_SENTENCIAS (t):
+    t_SENTENCIA (t_FIN_DE_LINEA)(t_SENTENCIAS) | t_SENTENCIA(t_FIN_DE_LINEA)
+
+
+
+
 
 
 # Define a function to handle errors
@@ -111,5 +106,4 @@ lexer = lex.lex()
 Raw = input('WRITE:' )
 lexer.input(Raw)
 for tok in lexer:
-    if tok.type == "ID":
-        print(f"Matched TOKEN: {tok.type}: {tok.value}")
+    print(f"Matched TOKEN: {tok.type}: {tok.value}")
