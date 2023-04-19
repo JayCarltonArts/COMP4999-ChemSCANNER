90004f4adb52a731274fb182391ea84154648d72
JayCarltonArts <99850395+JayCarltonArts@users.noreply.github.com>
2023-04-19 12:57:59 -0400

    ADDED SOME PARSER FUNCTIONS
    Las funciones no funcionan como deben pero si corres esto te va dar un archivo donde hace el processo de LR paso por paso para analizar sintacticamente el texto de error cases.

--- SCANNER TEST.py
+++ SCANNER TEST.py
@@ -1,13 +1,12 @@
 import ply.lex as lex
+import ply.yacc as yacc
 from ply.lex import Token
 import re
 # Define the list of token names
-tokens = (
-    'ID',
+tokens = ( 'ID',
 #    'IDCONT',
-    'LETRA',
-    'DIGITO',
     'VALENCIA',
+    'ENLACE',
     'TIPO',
     'ASIGNACION',
     'FIN_DE_LINEA',
@@ -21,14 +20,14 @@ tokens = (
     # 'SENTENCIAS',
     # 'DEFINICION_DEL_MODELO',
     # 'MODELO_MOLECULAR',
-    #'ELEMENTO',
+    # 'ELEMENTO',
     # 'CADENA_MOLECULAR',
     # 'CADENA_ATOMICA',
-    # 'GRUPO_FUNCIONAL',
+     'GRUPO_FUNCIONAL',
     # 'GRUPO_FUNCIONAL_SUPERIOR',
     # 'GRUPO_FUNCIONAL_INFERIOR',
     # 'COMPUESTO',
-    # 'COMPUESTOS',
+     'COMPUESTOS',
     # 'MODELO_GRUPO_FUNCIONAL'
     'PARAENTESIS_IZQ',
     'PARAENTESIS_DER',
@@ -37,8 +36,12 @@ tokens = (
 
 
 
-# Define a regular expression 
 
+# Define a regular expression
+ 
+def t_ASIGNACION(t):
+    r':='
+    return t
 def t_TIPO(t):
     r'modelo'
     return t
@@ -61,17 +64,56 @@ def t_VALENCIA(t):
 
 # Define the regular expression for each token
 t_FIN_DE_LINEA=r'\;|\:'
-t_ASIGNACION=r'\-|\:\:|\:|\='
+t_ID= r'[a-zA-Z][a-zA-Z0-9_]*'
+t_ENLACE=r'\-|\:\:|\:|\='
 t_PARAENTESIS_IZQ= r'\('
 t_PARAENTESIS_DER= r'\)'
-t_ID=r'[A-Za-z]+\d*'    
+
 
 
 
 # Definir las reglas de produccion
-'''
-t_ELEMENTO = r'(' + t_ELEMENTO_QUIMICO + r')(' + t_VALENCIA + r')?'
-t_IDCONT = r'\w+|(\w+)\w+|\d+|(\d+)\w+'
+
+def p_S(p):
+    '''S : PALABRAS_RESERVADAS SENTENCIAS PALABRAS_RESERVADAS'''
+
+def p_SENTENCIAS(p):
+    '''SENTENCIAS : SENTENCIA FIN_DE_LINEA SENTENCIAS	
+                  | SENTENCIA FIN_DE_LINEA'''
+
+def p_SENTENCIA(p):
+    '''SENTENCIA : PALABRAS_RESERVADAS variable PALABRAS_RESERVADAS TIPO
+                 | ASIGNACION
+                 | OPERACION PARAENTESIS_IZQ variable PARAENTESIS_DER'''
+
+def p_ASIGNACION(p):
+    '''variable : MODELO_MOLECULAR'''   
+    p[0] = p[1] 
+
+def p_COMPUESTO(p):
+    '''COMPUESTO : ELEMENTO_QUIMICO'''
+
+def p_MODELO_MOLECULAR(p):
+    '''MODELO_MOLECULAR : ELEMENTO_QUIMICO
+                        | ELEMENTO_QUIMICO VALENCIA
+                        | ELEMENTO GRUPO_FUNCIONAL 
+                        | COMPUESTO GRUPO_FUNCIONAL
+                        | COMPUESTO ELEMENTO GRUPO_FUNCIONAL
+                        | COMPUESTO COMPUESTO COMPUESTOS'''
+
+def p_variable(p):
+    '''variable : ID'''
+
+def p_ELEMENTO(p):
+    ''' ELEMENTO : ELEMENTO_QUIMICO
+            | ELEMENTO_QUIMICO VALENCIA '''
+
+def p_DECLARACION_DE_VARIABLE(p):
+    '''variable : PALABRAS_RESERVADAS variable PALABRAS_RESERVADAS TIPO FIN_DE_LINEA'''
+    
+
+    
+'''t_IDCONT = r'\w+|(\w+)\w+|\d+|(\d+)\w+'
 
 
 
@@ -99,18 +141,36 @@ t_SENTENCIAS=t_SENTENCIA + r'(' + t_FIN_DE_LINEA+r')+'+ r'|' +t_SENTENCIA+r'('+t
 
 # Define a function to handle errors
 def t_error(t):
-    print(f"Error: Illegal character 't.value[0]'")
-    t.lexer.skip(1)
+     print(f"Error: Illegal character 't.value[0]'")
+     t.lexer.skip(1)
 # Define a rule to handle whitespace
 t_ignore = ' \t\n'
+
+# Error rule for syntax errors
+def p_error(p):
+    raise SyntaxError(f"Syntax error at line {p.lineno}, position {p.lexpos}: Unexpected token {p.value}")
+    
 # Build the lexer
 lexer = lex.lex()
 
-# Test the lexer
+# Build the parser
+parser = yacc.yacc(debug=True)
+
+# # Test the lexer
 with open ('error_cases.txt','r') as file:
-    i = file.read()
+     i = file.read()
 
 lexer.input(i)
 for tok in lexer:
-    print(f"Matched TOKEN: {tok.type}: {tok.value}")
+     print(tok)
 
+
+while True:
+    try:
+       with open ('error_cases.txt','r') as file:
+            i = file.read()
+    except EOFError:
+       break
+    if not i: continue
+    result = parser.parse(i)
+    print(result)
