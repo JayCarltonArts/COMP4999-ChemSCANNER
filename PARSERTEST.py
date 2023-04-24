2d21885df747ea22ef9eb3a4423d8c71131f290f
JayCarltonArts <99850395+JayCarltonArts@users.noreply.github.com>
2023-04-24 16:48:10 +0000

    UPDATE: PARSER CAN RUN FULL GRAMMER, ERROR CASE CANNOT HAVE : AS FIN DE LINEA, TWO PARSER OUTS ARE CREATED FROM PARSERTEST

--- PARSERTEST.py
+++ PARSERTEST.py
@@ -5,19 +5,19 @@ import SCANNERTEST
 
 tokens = ( 
     'ID',
-    # 'VALENCIA',
-    # 'ENLACE',
+     'VALENCIA',
+     'ENLACE',
     'TIPO',
     'ASIGNACION',
      'FIN_DE_LINEA',
-    # 'ELEMENTO_QUIMICO',
+     'ELEMENTO_QUIMICO',
     'OPERACION',
     'PARAENTESIS_IZQ',
     'PARAENTESIS_DER',
     'PALABRAS_RESERVADAS',
-    'INICIO', 'DEFINA', 'COMO', 'FIN'
-#     'COR_IZQ',
-#     'COR_DER'
+    'INICIO', 'DEFINA', 'COMO', 'FIN',
+     'COR_IZQ',
+     'COR_DER'
  )
 
 parsed_tokens = []
@@ -59,9 +59,9 @@ def p_sentencias(p):
 # | OPERACION (ID) [function operation]
 def p_sentencia(p):
     '''sentencia : DEFINA ID COMO TIPO
-                 | ID ASIGNACION ID
+                 | ID ASIGNACION modelo_molecular
                  | OPERACION PARAENTESIS_IZQ ID PARAENTESIS_DER'''
-                 
+    
     
       # Extract the types of the symbols from the slice
     symbol_types = [symbol.type for symbol in p.slice[1:]]
@@ -69,6 +69,126 @@ def p_sentencia(p):
     # Convert the list of types to a string
     symbol_types_str = ' '.join(symbol_types)
     parsed_tokens.append((p.slice[0].type, symbol_types_str))
+
+#<COMPUESTO>::=	"<ELEMENTO_QUIMICO>	|	<ELEMENTO_QUIMICO>	<VALENCIA>	|	<ELEMENTO>	<GRUPO_FUNCIONAL>	|	<ELEMENTO>	<GRUPO_FUNCIONAL>	<ENLACE>	|	<ELEMENTO>	<ENLACE>
+def p_compuesto(p):
+    '''compuesto : ELEMENTO_QUIMICO
+                 | ELEMENTO_QUIMICO VALENCIA
+                 | elemento grupo_funcional
+                 | elemento grupo_funcional ENLACE
+                 | elemento ENLACE'''
+      # Extract the types of the symbols from the slice
+    symbol_types = [symbol.type for symbol in p.slice[1:]]
+
+    # Convert the list of types to a string
+    symbol_types_str = ' '.join(symbol_types)
+    parsed_tokens.append((p.slice[0].type, symbol_types_str))
+
+
+#<COMPUESTOS>::=	<COMPUESTO>	<COMPUESTOS>	|	<COMPUESTO>
+def p_compuestos(p):
+    '''compuestos : compuesto compuestos
+                 | compuesto'''
+      # Extract the types of the symbols from the slice
+    symbol_types = [symbol.type for symbol in p.slice[1:]]
+
+    # Convert the list of types to a string
+    symbol_types_str = ' '.join(symbol_types)
+    parsed_tokens.append((p.slice[0].type, symbol_types_str))
+                 
+
+#<MODELO_MOLECULAR>::=	<ELEMENTO_QUIMICO>	|	<ELEMENTO_QUIMICO>	<VALENCIA>	|	<ELEMENTO>	<GRUPO_FUNCIONAL>	|	<COMPUESTO>	<GRUPO_FUNCIONAL>	|	<COMPUESTO>	<ELEMENTO>	<GRUPO_FUNCIONAL>	|	<COMPUESTO>	<COMPUESTO>	<COMPUESTOS>
+def p_modelo_molecular(p):
+    '''modelo_molecular : ELEMENTO_QUIMICO
+                        | ELEMENTO_QUIMICO VALENCIA
+                        | elemento grupo_funcional 
+                        | compuesto grupo_funcional
+                        | compuesto elemento grupo_funcional
+                        | compuesto compuesto compuestos'''
+      # Extract the types of the symbols from the slice
+    symbol_types = [symbol.type for symbol in p.slice[1:]]
+
+    # Convert the list of types to a string
+    symbol_types_str = ' '.join(symbol_types)
+    parsed_tokens.append((p.slice[0].type, symbol_types_str))
+
+
+
+#<GRUPO_FUNCIONAL>::=	<GRUPO_FUNCIONAL_INFERIOR>	<GRUPO_FUNCIONAL_SUPERIOR>	|	<GRUPO_FUNCIONAL_SUPERIOR>	<GRUPO_FUNCIONAL_INFERIOR>	|	"("	<MODELO_GRUPO_FUNCIONAL>	")"	|	"["	<MODELO_GRUPO_FUNCIONAL>	"]"
+def p_grupo_funcional(p):
+    '''grupo_funcional : grupo_funcional_inferior grupo_funcional_superior
+                        | grupo_funcional_superior grupo_funcional_inferior
+                        | grupo_funcional_superior
+                        | grupo_funcional_inferior'''
+      # Extract the types of the symbols from the slice
+    symbol_types = [symbol.type for symbol in p.slice[1:]]
+
+    # Convert the list of types to a string
+    symbol_types_str = ' '.join(symbol_types)
+    parsed_tokens.append((p.slice[0].type, symbol_types_str))
+
+
+
+#<GRUPO_FUNCIONAL_SUPERIOR>::=	"("	<MODELO_GRUPO_FUNCIONAL>	")"
+
+def p_grupo_funcional_superior(p):
+    '''grupo_funcional_superior : PARAENTESIS_IZQ modelo_grupo_funcional PARAENTESIS_DER'''
+      # Extract the types of the symbols from the slice
+    symbol_types = [symbol.type for symbol in p.slice[1:]]
+
+    # Convert the list of types to a string
+    symbol_types_str = ' '.join(symbol_types)
+    parsed_tokens.append((p.slice[0].type, symbol_types_str))
+
+
+
+#<GRUPO_FUNCIONAL_INFERIOR>::=	"["	<MODELO_GRUPO_FUNCIONAL>	"]"
+
+def p_grupo_funcional_inferior(p):
+    '''grupo_funcional_inferior : COR_IZQ modelo_grupo_funcional COR_DER'''
+      # Extract the types of the symbols from the slice
+    symbol_types = [symbol.type for symbol in p.slice[1:]]
+
+    # Convert the list of types to a string
+    symbol_types_str = ' '.join(symbol_types)
+    parsed_tokens.append((p.slice[0].type, symbol_types_str))
+
+
+
+#<MODELO_GRUPO_FUNCIONAL>::=	<ENLACE>	<MODELO_MOLECULAR>	|	<ELEMENTO_QUIMICO>	|	<ELEMENTO_QUIMICO>	<VALENCIA>	|	<ELEMENTO>	<GRUPO_FUNCIONAL>	|	<COMPUESTO>	<ELEMENTO>	|	<COMPUESTO>	<ELEMENTO>	<GRUPO_FUNCIONAL>	|	<COMPUESTO>	<COMPUESTO>	<COMPUESTOS>
+def p_modelo_grupo_funcional(p):
+    '''modelo_grupo_funcional : ENLACE modelo_molecular
+                              | ELEMENTO_QUIMICO
+                              | ELEMENTO_QUIMICO VALENCIA
+                              | elemento grupo_funcional
+                              | compuesto elemento
+                              | compuesto elemento grupo_funcional
+                              | compuesto compuesto compuestos'''
+      # Extract the types of the symbols from the slice
+    symbol_types = [symbol.type for symbol in p.slice[1:]]
+
+    # Convert the list of types to a string
+    symbol_types_str = ' '.join(symbol_types)
+    parsed_tokens.append((p.slice[0].type, symbol_types_str))
+
+
+
+#<ELEMENTO>::=	"<ELEMENTO_QUIMICO>	|	<ELEMENTO_QUIMICO>	<VALENCIA>
+
+def p_elemento(p):
+    ''' elemento : ELEMENTO_QUIMICO
+            | ELEMENTO_QUIMICO VALENCIA '''
+      # Extract the types of the symbols from the slice
+    symbol_types = [symbol.type for symbol in p.slice[1:]]
+
+    # Convert the list of types to a string
+    symbol_types_str = ' '.join(symbol_types)
+    parsed_tokens.append((p.slice[0].type, symbol_types_str))
+
+
+
+
+
 
 # Error rule for syntax errors
 def p_error(p):
@@ -78,13 +198,13 @@ def p_error(p):
         parser.errok()
     else:
         print("Syntax error: Unexpected end of input")
-
+    
 
 
 
 
 # Build the parser
-parser = yacc.yacc(debug=True)
+parser = yacc.yacc(debug=False)
 
 # Test the Parser
 with open ('error_cases.txt','r') as file:
@@ -99,9 +219,11 @@ with open("parserout", "w") as outfile:
 
     # Build the table
     x = PrettyTable()
-    x.field_names = ["Pila", "Entrada"]
+    x.field_names = ["Izquierda", "Derecha"]
     for prod in parsed_tokens:
         x.add_row(prod)
 
     
     outfile.write(str(x))
+    
+    
